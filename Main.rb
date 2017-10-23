5f80ccd85f26eb3d72b2d2d4cddb596aa3a14f5d
Hamish C. Sams <Hammie221177@gmail.com>
2017-10-23 19:03:21 +0100

    works for any string of plus and minuses

--- Main.rb
+++ Main.rb
@@ -1,4 +1,21 @@
 puts "Enter your basic maths equation"
 equation = gets().chomp
 nums=equation.split(/\W+/)
-puts nums
+operand=equation.tr("0-9", "")
+operand=operand.split("")
+i=0
+operand.each do |x|
+
+  if x=="+"
+    nums[i]=nums[i].to_i+nums[i+1].to_i
+    nums[i+1]=nums[i]
+
+  elsif x=="-"
+    nums[i]=nums[i].to_i-nums[i+1].to_i
+    nums[i+1]=nums[i]
+
+  end
+  i +=1
+end
+length=nums.length
+puts nums[length-1]
