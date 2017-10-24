8f5c9ea99e2670a5a7cca7b5a75f6bf25ece9c48
Hamish C. Sams <Hammie221177@gmail.com>
2017-10-24 13:18:05 +0100

    Algorythm works inefficiently

--- Main.rb
+++ Main.rb
@@ -3,19 +3,92 @@ equation = gets().chomp
 nums=equation.split(/\W+/)
 operand=equation.tr("0-9", "")
 operand=operand.split("")
-i=0
-operand.each do |x|
-
-  if x=="+"
-    nums[i]=nums[i].to_i+nums[i+1].to_i
-    nums[i+1]=nums[i]
-
-  elsif x=="-"
-    nums[i]=nums[i].to_i-nums[i+1].to_i
-    nums[i+1]=nums[i]
-
+opCount=operand.length
+ans =0;
+while(equation.include?("*")or equation.include?("/"))
+for i in 0...opCount
+  if operand[i]=="*"
+    ans=nums[i].to_i*nums[i+1].to_i
+    equation=""
+    for ii in 0...opCount
+      if ii<i
+      equation += nums[ii].to_s
+      equation += operand[ii].to_s
+      elsif ii>i
+        equation += operand[ii].to_s
+        equation += nums[ii+1].to_s
+      elsif
+        equation += ans.to_s
+      end
+    end
+    nums=equation.split(/\W+/)
+    operand=equation.tr("0-9", "")
+    operand=operand.split("")
+    opCount=operand.length
   end
-  i +=1
+  if operand[i]=="/"
+    ans=nums[i].to_i/nums[i+1].to_i
+    equation=""
+    for ii in 0...opCount
+      if ii<i
+        equation += nums[ii].to_s
+        equation += operand[ii].to_s
+      elsif ii>i
+        equation += operand[ii].to_s
+        equation += nums[ii+1].to_s
+      elsif
+      equation += ans.to_s
+      end
+    end
+    nums=equation.split(/\W+/)
+    operand=equation.tr("0-9", "")
+    operand=operand.split("")
+    opCount=operand.length
+  end
 end
-length=nums.length
-puts nums[length-1]
+end
+equ="-"
+while(equation.include?("+")or equ.include?("-"))
+  for i in 0...opCount
+    if operand[i]=="+"
+      ans=nums[i].to_i+nums[i+1].to_i
+      equation=""
+      for ii in 0...opCount
+        if ii<i
+          equation += nums[ii].to_s
+          equation += operand[ii].to_s
+        elsif ii>i
+          equation += operand[ii].to_s
+          equation += nums[ii+1].to_s
+        elsif
+        equation += ans.to_s
+        end
+      end
+      nums=equation.split(/\W+/)
+      operand=equation.tr("0-9", "")
+      operand=operand.split("")
+      opCount=operand.length
+    end
+    if operand[i]=="-"
+      ans=nums[i].to_i-nums[i+1].to_i
+      equation=""
+      for ii in 0...opCount
+        if ii<i
+          equation += nums[ii].to_s
+          equation += operand[ii].to_s
+        elsif ii>i
+          equation += operand[ii].to_s
+          equation += nums[ii+1].to_s
+        elsif
+        equation += ans.to_s
+        end
+      end
+      nums=equation.split(/\W+/)
+      operand=equation.tr("0-9", "")
+      operand=operand.split("")
+      opCount=operand.length
+    end
+  end
+  equ = equation[1..-1]
+  end
+  puts equation
